b52b540a187dd811ce9d7af6ad77c92b0955de25
Pedro Franceschi <117317386+FranceschiPedro@users.noreply.github.com>
2026-02-03 14:02:27 +0000

    First set up

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
+""" 
 import os
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
@@ -38,7 +38,8 @@ def sitemap():
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
-
+    # person = User.query.get(1)
+    # print(person)
     response_body = {
         "msg": "Hello, this is your GET /user response "
     }
--- src/models.py
+++ src/models.py
@@ -1,19 +1,53 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
-
+from sqlalchemy import String, Boolean, Table, Column, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+ 
 db = SQLAlchemy()
+
+favorite_table = Table(
+    "favorites",
+    db.Model.metadata,
+    Column("user_id", ForeignKey("user.id"), primary_key = True), 
+    Column("character_id", ForeignKey("character.id"), primary_key = True)
+)
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    name: Mapped[str] = mapped_column(String(120))
+    favorites: Mapped[list["Character"]] = relationship(
+        "Character",
+        secondary = favorite_table,
+        back_populates = "favorited_by" #sincroniza ambos modelos, es un vinculo bidireccional.
+    )
 
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "favorites": self.favorites
             # do not serialize the password, its a security breach
         }
+
+class Character(db.Model):
+    id: Mapped[int] = mapped_column(primary_key = True)
+    name: Mapped[str] = mapped_column(String(120), nullable = False)
+    quote: Mapped[str] = mapped_column(String(120), nullable = False)
+    image: Mapped[str] = mapped_column(String(120), nullable = True)
+    favorited_by: Mapped[list["User"]] = relationship(
+        "User",
+        secondary = favorite_table,
+        back_populates = "favorites" #sincroniza ambos modelos, es un vinculo bidireccional.
+    )
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "quote": self.quote,
+            "image": self.image
+        }
+    # After creating the new model, go to admin file and add a VIEW
+
